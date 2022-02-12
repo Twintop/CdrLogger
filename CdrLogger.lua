a5431eca64b02dc2c22bed24632c9fd52a931799
David C. Uhrig <david@twintop-tahoe.com>
2022-02-13 03:17:02 +1100

    first commit

---
+++ CdrLogger.lua
@@ -0,0 +1,133 @@
+local _, CdrLogger = ...
+local trackedSpells = {}
+
+CdrLogger = CdrLogger or {}
+CdrLogger.Data = CdrLogger.Data or {}
+
+if CdrLoggerSettings then
+    CdrLogger.Data.settings = CdrLogger.Functions:MergeSettings(CdrLogger.Functions:GetDefaultSettings(), CdrLoggerSettings)
+else
+    CdrLogger.Data.settings = CdrLogger.Functions:GetDefaultSettings()
+end
+
+_, CdrLogger.Data.className, _ = UnitClass("player")
+CdrLogger.Data.specName = nil
+CdrLogger.Data.specId = 0
+CdrLogger.Data.characterGuid = UnitGUID("player")
+CdrLogger.Data.enabled = false
+
+-- Frames
+local containerFrame = CreateFrame("Frame", "CdrLoggerFrame", UIParent, "BackdropTemplate")
+local combatFrame = CreateFrame("Frame", nil, containerFrame, "BackdropTemplate")
+local timerFrame = CreateFrame("Frame", nil, containerFrame)
+timerFrame.sinceLastUpdate = 0
+
+function CdrLogger:EventRegistration()
+    if CdrLogger.Data.enabled then
+        timerFrame:SetScript("OnUpdate", function(self, sinceLastUpdate) timerFrame:onUpdate(sinceLastUpdate) end)
+        combatFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+        print("CDR logging enabled.")
+    else
+        timerFrame:SetScript("OnUpdate", nil)
+        combatFrame:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+        print("CDR logging disabled.")
+    end
+end
+
+function timerFrame:onUpdate(sinceLastUpdate)
+    local currentTime = GetTime()
+    self.sinceLastUpdate = self.sinceLastUpdate + sinceLastUpdate
+    if self.sinceLastUpdate >= 0.05 then -- in seconds
+        for x, v in pairs(trackedSpells) do
+---@diagnostic disable-next-line: redundant-parameter
+            local startTime, duration, _, _ = GetSpellCooldown(trackedSpells[x].id)
+            if startTime == 0 then
+                local actualDuration = currentTime - trackedSpells[x].startTime
+                local durationDelta = currentTime - trackedSpells[x].originalEndTime
+                local updateDurationDelta = currentTime - trackedSpells[x].latestEndTime
+                print("OFF CD: " .. trackedSpells[x].name .. " -- Actual = " .. actualDuration .. " | Original Delta = " .. durationDelta .. " | LatestChange Delta = " .. updateDurationDelta)
+                trackedSpells[x] = nil
+            else
+                local previousRemainingTime = trackedSpells[x].latestEndTime - currentTime
+
+                trackedSpells[x].lastUpdatedTime = currentTime
+                trackedSpells[x].latestDuration = duration
+                trackedSpells[x].latestEndTime = duration + startTime
+
+                local originalRemainingTime = trackedSpells[x].originalEndTime - currentTime
+                local latestRemainingTime = trackedSpells[x].latestEndTime - currentTime
+
+                if previousRemainingTime ~= latestRemainingTime then
+                    print("CD CHANGE: " .. trackedSpells[x].name .. " -- " .. previousRemainingTime .. "-" .. latestRemainingTime .. " = " .. previousRemainingTime - latestRemainingTime)
+                end
+            end
+        end
+        self.sinceLastUpdate = 0
+    end
+end
+
+combatFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+combatFrame:SetScript("OnEvent", function(self, event, ...)
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+		local currentTime = GetTime()
+        local time, type, _, sourceGUID, sourceName, _, _, destGUID, destName, _, _, spellId, spellName = CombatLogGetCurrentEventInfo()
+        
+        if sourceGUID == CdrLogger.Data.characterGuid then
+            local spells = CdrLogger.Data.settings[CdrLogger.Data.className][CdrLogger.Data.specName].spells
+
+            for x, v in pairs(spells) do
+                if spellId == tonumber(v) then
+                    if type == "SPELL_CAST_SUCCESS" then
+                        C_Timer.After(0.05, function()
+---@diagnostic disable-next-line: redundant-parameter
+                            local startTime, duration, _, _ = GetSpellCooldown(spellId)
+                            local name, _, icon = GetSpellInfo(spellId)
+                            trackedSpells[spellId] = {
+                                id = spellId,
+                                name = name,
+                                icon = icon,
+                                startTime = startTime,
+                                originalDuration = duration,
+                                originalEndTime = startTime + duration,
+                                latestDuration = duration,
+                                latestEndTime = duration + startTime,
+                                lastUpdatedTime = currentTime
+                            }
+                            print("ON CD: " .. name .. " -- " .. duration .. "|" .. startTime+duration)
+                        end)
+                    end
+                end
+            end
+        end
+    end
+end)
+
+
+containerFrame:RegisterEvent("PLAYER_LOGOUT") -- Fired when about to log out
+containerFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+containerFrame:RegisterEvent("PLAYER_TALENT_UPDATE")
+containerFrame:RegisterEvent("ADDON_LOADED")
+containerFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+containerFrame:SetScript("OnEvent", function(self, event, arg1, ...)
+    if (event == "ADDON_LOADED" and arg1 == "CdrLogger") then
+        if not CdrLogger.Data.loaded then
+            CdrLogger.Data.loaded = true
+            SLASH_CDRLOGGER1 = "/cdrl"
+            SLASH_CDRLOGGER2 = "/cdrlog"
+            SLASH_CDRLOGGER3 = "/cdrlogger"
+            CdrLogger:EventRegistration()
+        end
+    end
+
+    if CdrLogger.Data.loaded then
+        if event == "PLAYER_LOGOUT" then
+            CdrLoggerSettings = CdrLogger.Data.settings
+            return
+        end
+       
+        if event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_TALENT_UPDATE" or event == "PLAYER_SPECIALIZATION_CHANGED" then
+            CdrLogger.Data.specId = GetSpecialization()
+            CdrLogger.Data.specName = CdrLogger.Functions:LookupSpecializationName(CdrLogger.Data.className, CdrLogger.Data.specId)
+        end
+    end
+end)
---
+++ Functions.lua
@@ -0,0 +1,335 @@
+local _, CdrLogger = ...
+CdrLogger.Functions = CdrLogger.Functions or {}
+
+function CdrLogger.Functions:TableLength(T)
+	local count = 0
+	if T ~= nil then
+		local _
+		for _ in pairs(T) do
+			count = count + 1
+		end
+	end
+	return count
+end
+
+function CdrLogger.Functions:GetDefaultSettings()
+    local defaultSettings = {
+        DEATHKNIGHT = {
+            BLOOD = {
+                spells = {}
+            },
+            FROST = {
+                spells = {}
+            },
+            UNHOLY = {
+                spells = {}
+            },
+        },
+        DEMONHUNTER = {
+            HAVOC = {
+                spells = {}
+            },
+            VENGENCE = {
+                spells = {}
+            },
+        },
+        DRUID = {
+            BALANCE = {
+                spells = {}
+            },
+            FERAL = {
+                spells = {}
+            },
+            GUARDIAN = {
+                spells = {}
+            },
+            RESTORATION = {}
+        },
+        HUNTER = {
+            BEASTMASTERY = {
+                spells = {}
+            },
+            MARKSMANSHIP = {
+                spells = {}
+            },
+            SURVIVAL = {}
+        },
+        MAGE = {
+            ARCANE = {
+                spells = {}
+            },
+            FIRE = {
+                spells = {}
+            },
+            FROST = {}
+        },
+        MONK = {
+            BREWMASTER = {
+                spells = {}
+            },
+            MISTWEAVER = {
+                spells = {}
+            },
+            WINDWALKER = {}
+        },
+        PALADIN = {
+            HOLY = {
+                spells = {}
+            },
+            PROTECTION = {
+                spells = {}
+            },
+            RETRIBUTION = {}
+        },
+        PRIEST = {
+            DISCIPLINE = {
+                spells = {}
+            },
+            HOLY = {
+                spells = {
+                    "2050",
+                    "34861",
+                    "88625",
+                    "265202"
+                }
+            },
+            SHADOW = {
+                spells = {}
+            }
+        },
+        ROGUE = {
+            ASSASSINATION = {
+                spells = {}
+            },
+            OUTLAW = {
+                spells = {}
+            },
+            SUBTLETY = {}
+        },
+        SHAMAN = {
+            ELEMENTAL = {
+                spells = {}
+            },
+            ENHANCEMENT = {
+                spells = {}
+            },
+            RESTORATION = {
+                spells = {}
+            }
+        },
+        WARLOCK = {
+            AFFLICTION = {
+                spells = {}
+            },
+            DEMONOLOGY = {
+                spells = {}
+            },
+            DESTRUCTION = {}
+        },
+        WARRIOR = {
+            ARMS = {
+                spells = {}
+            },
+            FURY = {
+                spells = {}
+            },
+            PROTECTION = {}
+        }
+    }
+    return defaultSettings
+end
+
+function CdrLogger.Functions:MergeSettings(settings, user)
+	if settings == nil and user == nil then
+		return {}
+	elseif settings == nil then
+		return user
+	elseif user == nil then
+		return settings
+	end
+
+	for k, v in pairs(user) do        
+        if (type(v) == "table") and (type(settings[k] or false) == "table") then
+            CdrLogger.Functions:MergeSettings(settings[k], user[k])
+        else
+            settings[k] = v
+        end
+    end
+    return settings
+end
+
+function CdrLogger.Functions:AddTrackedSpell(className, specName, spellId)
+    local exists = false
+    local t = CdrLogger.Data.settings[className][specName].spells
+    for x = 1, #t do
+        if t[x] == spellId then
+            exists = true
+            break
+        end
+    end
+
+    if not exists then
+        table.insert(CdrLogger.Data.settings[className][specName].spells, spellId)
+        return true
+    end
+    return false
+end
+
+function CdrLogger.Functions:RemoveTrackedSpell(className, specName, spellId)
+    local t = CdrLogger.Data.settings[className][specName].spells
+    for x = 1, #t do
+        if t[x] == spellId then
+            table.remove(CdrLogger.Data.settings[className][specName].spells, x)
+            return true
+        end
+    end
+    return false
+end
+
+function CdrLogger.Functions:LookupSpecializationName(className, specId)
+    if className == "DEATHKNIGHT" then
+        if specId == 1 then
+            return "BLOOD"
+        elseif specId == 2 then
+            return "FROST"
+        elseif specId == 3 then
+            return "UNHOLY"
+        end
+    elseif className == "DEMONHUNTER" then
+        if specId == 1 then
+            return "HAVOC"
+        elseif specId == 2 then
+            return "VENGENCE"
+        end
+    elseif className == "DRUID" then
+        if specId == 1 then
+            return "BALANCE"
+        elseif specId == 2 then
+            return "FERAL"
+        elseif specId == 3 then
+            return "GUARDIAN"
+        elseif specId == 4 then
+            return "RESTORATION"
+        end
+    elseif className == "HUNTER" then
+        if specId == 1 then
+            return "BEASTMASTERY"
+        elseif specId == 2 then
+            return "MARKSMANSHIP"
+        elseif specId == 3 then
+            return "SURVIVAL"
+        end
+    elseif className == "MAGE" then
+        if specId == 1 then
+            return "ARCANE"
+        elseif specId == 2 then
+            return "FIRE"
+        elseif specId == 3 then
+            return "FROST"
+        end
+    elseif className == "MONK" then
+        if specId == 1 then
+            return "BREWMASTER"
+        elseif specId == 2 then
+            return "MISTWEAVER"
+        elseif specId == 3 then
+            return "WINDWALKER"
+        end
+    elseif className == "PALADIN" then
+        if specId == 1 then
+            return "HOLY"
+        elseif specId == 2 then
+            return "PROTECTION"
+        elseif specId == 3 then
+            return "RETRIBUTION"
+        end
+    elseif className == "PRIEST" then
+        if specId == 1 then
+            return "DISCIPLINE"
+        elseif specId == 2 then
+            return "HOLY"
+        elseif specId == 3 then
+            return "SHADOW"
+        end
+    elseif className == "ROGUE" then
+        if specId == 1 then
+            return "ASSASSINATION"
+        elseif specId == 2 then
+            return "OUTLAW"
+        elseif specId == 3 then
+            return "SUBTLETY"
+        end
+    elseif className == "SHAMAN" then
+        if specId == 1 then
+            return "ELEMENTAL"
+        elseif specId == 2 then
+            return "ENHANCEMENT"
+        elseif specId == 3 then
+            return "RESTORATION"
+        end
+    elseif className == "WARLOCK" then
+        if specId == 1 then
+            return "AFFLICTION"
+        elseif specId == 2 then
+            return "DEMONOLOGY"
+        elseif specId == 3 then
+            return "DESTRUCTION"
+        end
+    elseif className == "WARRIOR" then
+        if specId == 1 then
+            return "ARMS"
+        elseif specId == 2 then
+            return "FURY"
+        elseif specId == 3 then
+            return "PROTECTION"
+        end
+    end
+    return nil
+end
+
+function CdrLogger.Functions:ParseCmdString(msg)
+	if msg then
+		while (strfind(msg,"  ") ~= nil) do
+			msg = string.gsub(msg,"  "," ")
+		end
+		local a,b,c=strfind(msg,"(%S+)")
+		if a then
+			return c,strsub(msg,b+2)
+		else
+			return "";
+		end
+	end
+end
+
+function SlashCmdList.CDRLOGGER(msg)
+    local cmd, subcmd = CdrLogger.Functions:ParseCmdString(msg);
+    if cmd == "add" then
+        local spellId = CdrLogger.Functions:ParseCmdString(subcmd)
+        local result = CdrLogger.Functions:AddTrackedSpell(CdrLogger.Data.className, CdrLogger.Data.specName, spellId)
+
+        if result then
+            print("Added spell #", spellId, "to", CdrLogger.Data.specName, CdrLogger.Data.className, ".")
+        else
+            print("Failed to add spell #", spellId, "to", CdrLogger.Data.specName, CdrLogger.Data.className, ".")
+        end
+    elseif cmd == "remove" then
+        local spellId = CdrLogger.Functions:ParseCmdString(subcmd)
+        local result = CdrLogger.Functions:RemoveTrackedSpell(CdrLogger.Data.className, CdrLogger.Data.specName, spellId)
+
+        if result then
+            print("Removed spell #", spellId, "from", CdrLogger.Data.specName, CdrLogger.Data.className, ".")
+        else
+            print("Failed to remove spell #", spellId, "from", CdrLogger.Data.specName, CdrLogger.Data.className, ".")
+        end
+    elseif cmd == "start" or cmd == "on" then
+        CdrLogger.Data.enabled = true
+        CdrLogger:EventRegistration()
+    elseif cmd == "stop" or cmd == "off" then
+        CdrLogger.Data.enabled = false
+        CdrLogger:EventRegistration()
+    elseif cmd == "list" then
+
+    else
+        print("Cooldown Reduction Logger (/cdrl) -- Available commands: on, off, add, remove, list")
+    end
+end
